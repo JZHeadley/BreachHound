ae36ea01dcb4cae3118113e60486fd52a6f16098
hudginspj <hudginspj@vcu.edu>
2017-04-08 12:21:27 -0400

    Added sample cluster

--- model/analysis.js
+++ model/analysis.js
@@ -3,24 +3,24 @@
  */
 
 
-
+//Display all these, sort by account number
 function getSampleCluster() {
     var p1 = {
-        _id: randomDigits(24),
+        _id: '123456789012345678901234',
         type: "merchant",
         merchant_id: '123456789012345678901234',
         payer_id: '123456789012345678901234',
-        purchase_date: "2017-04-18",
+        purchase_date: "2017-04-18", //use this
         amount: 50,
         status: "completed",
         medium: "balance",
-        account_number: "1234567890123456",
-        merchant_name: "Joes house of cats",
-        geocode : {lat: 37.53, lng: -77.4},
-        confirmed_fraud : 0
+        account_number: "1234567890123456",  //display
+        merchant_name: "Joes house of cats", //display
+        geocode : {lat: 37.53, lng: -77.4},  //use this
+        confirmed_fraud : 0  //color red/yellow
     };
-    var p1 = {
-        _id: randomDigits(24),
+    var p2 = {
+        _id: '123456789012345678901235',
         type: "merchant",
         merchant_id: '123456789012345678901234',
         payer_id: '123456789012345678901234',
@@ -28,11 +28,26 @@ function getSampleCluster() {
         amount: 50,
         status: "completed",
         medium: "balance",
-        account_number: "1234567890123456",
+        account_number: "1234567890123477",
         merchant_name: "Joes house of cats",
         geocode : {lat: 37.539, lng: -77.49},
+        confirmed_fraud : 1
+    };
+    var p3 = {
+        _id: '123456789012345678901236',
+        type: "merchant",
+        merchant_id: '123456789012345678901234',
+        payer_id: '123456789012345678901234',
+        purchase_date: "2017-04-18",
+        amount: 50,
+        status: "completed",
+        medium: "balance",
+        account_number: "1234567890123477",
+        merchant_name: "Bobs mitten parlor",
+        geocode : {lat: 37.539, lng: -77.40},
         confirmed_fraud : 0
     };
+    return [p2, p1, p3];
 }
 
 function analyze(arrayOfDictionaries){
@@ -43,4 +58,7 @@ function analyze(arrayOfDictionaries){
 }
 
 
-
+module.exports = {
+    getSampleCluster: getSampleCluster,
+    analyze: analyze
+}
