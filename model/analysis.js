c92ffcf8bbafac69b14bf5847e712fffe479fb1a
hudginspj <hudginspj@vcu.edu>
2017-04-08 12:06:45 -0400

    More sample data

--- model/analysis.js
+++ model/analysis.js
@@ -3,8 +3,44 @@
  */
 
 
+
+function getSampleCluster() {
+    var p1 = {
+        _id: randomDigits(24),
+        type: "merchant",
+        merchant_id: '123456789012345678901234',
+        payer_id: '123456789012345678901234',
+        purchase_date: "2017-04-18",
+        amount: 50,
+        status: "completed",
+        medium: "balance",
+        account_number: "1234567890123456",
+        merchant_name: "Joes house of cats",
+        geocode : {lat: 37.53, lng: -77.4},
+        confirmed_fraud : 0
+    };
+    var p1 = {
+        _id: randomDigits(24),
+        type: "merchant",
+        merchant_id: '123456789012345678901234',
+        payer_id: '123456789012345678901234',
+        purchase_date: "2017-04-18",
+        amount: 50,
+        status: "completed",
+        medium: "balance",
+        account_number: "1234567890123456",
+        merchant_name: "Joes house of cats",
+        geocode : {lat: 37.539, lng: -77.49},
+        confirmed_fraud : 0
+    };
+}
+
 function analyze(arrayOfDictionaries){
     console.log("Array of dictionaries:");
     console.log(arrayOfDictionaries);
     return;
+
 }
+
+
+
--- model/testData.js
+++ model/testData.js
@@ -2,16 +2,19 @@
  * Created by pjhud on 4/8/2017.
  */
 
-var addressPool = {};
-
+var addressPool = [];
 var merchants = {};
+var merchantsByState = {};
 var accounts = {};
+var purchases = {};
+var customers = {};
 
 
-var merchantsByState = {}
-
-
-
+/*x: Math.random() * 90,
+    y: Math.random() * 90,
+    t: Math.random() * 100000,
+    d: Math.random() * 0.5/Math.random(),
+    m: merchants[parseInt(Math.random() * merchants.length)]*/
 
 
 function randomElement(arr){
@@ -22,29 +25,99 @@ function randomKey(dict) {
     return randomElement(dict.keySet);
 }
 
-function genNormalPurch() {
-    var p = {
-        _id: parseInt(Math.random * 100000000).toString(),
-        type: "merchant",
-        merchant_id: randomKey(merchants),
-        payer_id: randomKey(accounts),
+function randomDigits(numDigits) {
+    n = parseInt(Math.random() * Math.pow(10, n));
+    return n.toString();
+}
 
-        x: Math.random() * 90,
-        y: Math.random() * 90,
-        t: Math.random() * 100000,
-        d: Math.random() * 0.5/Math.random(),
-        m: merchants[parseInt(Math.random() * merchants.length)]
-    };
-    return p;
+function randomName() {
+    return "Bob";
+}
+
+function randomMerchantName() {  //TODO
+    return "Joe's Shoe Parlor";
 }
 
 
-var testAddress = {
+function geoCodeByAddress(address) {
+    var cd = {
+        lat: 35.7,
+        lng: -43.2
+    };
+    return cd;
+}
 
+function genNewCustomer() {
+    var c = {
+        _id: randomDigits(24),
+        first_name: randomName(),
+        last_name: randomName(),
+        category: ["Food"], //TODO
+        address: randomElement(addressPool)
+    };
+    customers[c._id] = c;
+    return c;
+}
+
+function genNewAccount() {
+    var a = {
+        _id: randomDigits(24),
+        type: "Credit Card",
+        nickname: randomDigits(4),
+        rewards: 0,
+        balance: 999999999999,
+        account_number: random_digits(24),
+        customer_id : randomKey(customers)
+    };
+    customers[c._id] = c;
+    return c;
+}
+
+function genNewMerchant() {
+    addr = randomElement(addressPool)
+    var m = {
+        _id: randomDigits(24),
+        name: randomMerchantName(),
+        category: ["Food"], //TODO
+        address: addr,
+        geoCode: geoCodeFromAddress(addr)
+    };
+    merchants[m._id] = m;
+    return m;
+}
+
+
+function genNormalPurchase() {
+    var p = {
+        _id: randomDigits(24),
+        type: "merchant",
+        merchant_id: randomKey(merchants),
+        payer_id: randomKey(accounts),
+        purchase_date: "2017-04-18",
+        amount: Math.round(Math.random * 10000)/100,
+        status: "completed",
+        medium: "balance"
+    };
+    purchases[p._id] = p;
+    return p;
+}
+
+var testAddress = {
+    street_number: "1234",
+    street_name: "Aweseome St.",
+    city: "Richmond",
+    state: "VA",
+    zip: "23220"
 };
 
+addressPool.push(testAddress)
 
-
+module.exports = {
+    genNewCustomer: genNewCustomer,
+    genNewAccount: genNewAccount,
+    genNewMerchant: genNewMerchant,
+    genNormalPurchase: genNormalPurchase
+}
 
 
 
