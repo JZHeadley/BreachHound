b4efc5b62e541816325892536757cdb7e3bea043
parrycj <parrycj@vcu.edu>
2017-04-09 09:54:38 -0400

    Pretty

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,10 +1,25 @@
 /* Close when someone clicks on the overlay */
+
+
+
+function triggerNav() {
+
+        if(document.getElementById("tableOverlay").offsetWidth > 512) {
+            closeNav()
+        }
+        else{
+            openNav()
+        }
+}
+
 function closeNav() {
-        document.getElementById("tableOverlay").style.width = "40%";
-        document.getElementById("map").style.marginLeft = "40%"
+    //1280
+    document.getElementById("tableOverlay").style.width = "40%";
+    document.getElementById("map").style.marginLeft = "40%"
 }
 
 function openNav() {
+    //512
     document.getElementById("tableOverlay").style.width = "100%";
 }
 
