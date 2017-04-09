edcb599301ac2ea6bb758d54c38c3faa0c16cdf8
jzheadley <zephyrinkg@gmail.com>
2017-04-09 01:24:35 -0400

    file upload working

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,22 @@
 var express = require('express')
 var router = express.Router();
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now());
+    }
+});
+var upload = multer({storage: storage}).single('file');
 
 router.post('/', function (req, res, next) {
-
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    })
 });
 module.exports = router;
